f69e1b02c2657a86b635d46599a973a557e2180c
AndrewBellamy <andymanbellamy@gmail.com>
2017-09-19 23:21:23 +0930

    Second refactor, after initial alpha

--- app/src/main/java/com/example/thean/hitam/Home.java
+++ app/src/main/java/com/example/thean/hitam/Home.java
@@ -175,6 +175,7 @@ public class Home extends AppCompatActivity {
             calendar.set(Calendar.HOUR, 0);
             calendar.set(Calendar.MINUTE, 0);
             calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
             calendar.set(Calendar.HOUR_OF_DAY, 0);
 
             Long date = calendar.getTimeInMillis();
